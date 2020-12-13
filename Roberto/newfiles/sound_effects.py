9627084ccc0fde2ec4e669d497c9e3554631be43
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2020-12-12 17:25:01 -0800

    Update sound_effects.py

--- Roberto/newfiles/sound_effects.py
+++ Roberto/newfiles/sound_effects.py
@@ -1,7 +1,9 @@
 #load sounds
 self.arrow_sound = arcade.load_sound("music/arrow_sound.wav")
 self.sword_sound = arcade.load_sound("music/sword_sound.wav")
+self.switch_sound = arcade.load_sound("music/switch_sound.wav")
         
 # play sounds
 arcade.play_sound(self.sword_sound)
 arcade.play_sound(self.arrow_sound)
+arcade.play_sound(self.switch_sound)
