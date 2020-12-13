a7a4eeb810025b0b8adb35b7a3a2904c83c099cf
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2020-12-12 17:35:42 -0800

    Update sound_effects.py

--- Roberto/newfiles/sound_effects.py
+++ Roberto/newfiles/sound_effects.py
@@ -4,6 +4,7 @@ self.sword_sound = arcade.load_sound("music/sword_sound.wav")
 self.switch_sound = arcade.load_sound("music/switch_sound.wav")
 self.angry_peanut_death_sound = arcade.load_sound("music/angry_peanut_death_sound.wav")
 self.boss_death_sound = arcade.load_sound("music/boss_death_sound.wav")
+self.damage_taken_player_sound = arcade.load_sound("music/damage_taken_player_sound.wav")
         
 # play sounds
 arcade.play_sound(self.sword_sound)
@@ -11,5 +12,6 @@ arcade.play_sound(self.arrow_sound)
 arcade.play_sound(self.switch_sound)
 arcade.play_sound(self.angry_peanut_death_sound)
 arcade.play_sound(self.boss_death_sound)
+arcade.play_sound(self.damage_taken_player_sound)
 
 
