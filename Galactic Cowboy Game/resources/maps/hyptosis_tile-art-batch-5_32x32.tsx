083a29b14487c5d635c9184c0a50039b383a3a07
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2020-12-14 19:37:19 -0800

    Add files via upload

---
+++ Galactic Cowboy Game/resources/maps/hyptosis_tile-art-batch-5_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="hyptosis_tile-art-batch-5_32" tilewidth="32" tileheight="32" tilecount="722" columns="19">
+ <image source="../images for the game/hyptosis_tile-art-batch-5.png" width="636" height="1219"/>
+</tileset>
---
+++ Galactic Cowboy Game/resources/maps/world parts.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="world parts " tilewidth="16" tileheight="16" spacing="16" margin="16" tilecount="900" columns="30">
+ <image source="../images for the game/hyptosis_til-art-batch-2.png" width="960" height="960"/>
+</tileset>
